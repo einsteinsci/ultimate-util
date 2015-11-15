bd3371d5dd9a89bef51dc2c19842e487958e1339
einsteinsci <einsteinsci@gmail.com>
2015-11-15 14:37:13 -0800

    Documented number and reflection utils

--- UltimateUtil/UltimateUtil/NumberUtils.cs
+++ UltimateUtil/UltimateUtil/NumberUtils.cs
@@ -7,55 +7,131 @@ using System.Threading.Tasks;
 
 namespace UltimateUtil
 {
+	/// <summary>
+	/// Various utilities involving numeric types
+	/// </summary>
 	public static class NumberUtils
 	{
+		/// <summary>
+		/// Returns whether a number is between a range of values, inclusively
+		/// </summary>
+		/// <typeparam name="T">Type of value to compare</typeparam>
+		/// <param name="value">Value to compare</param>
+		/// <param name="lower">Lower bound</param>
+		/// <param name="upper">Upper bound</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="value"/> is between <paramref name="lower"/> and
+		/// <paramref name="upper"/> inclusively, <c>false</c> if not
+		/// </returns>
 		public static bool IsBetween<T>(this T value, T lower, T upper) where T : IComparable<T>
 		{
 			return value.CompareTo(lower) >= 0 && value.CompareTo(upper) <= 0;
 		}
+		/// <summary>
+		/// Returns whether a number is between a range of values, exclusively
+		/// </summary>
+		/// <typeparam name="T">Type of value to compare</typeparam>
+		/// <param name="value">Value to compare</param>
+		/// <param name="lower">Lower bound</param>
+		/// <param name="upper">Upper bound</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="value"/> is between <paramref name="lower"/> and
+		/// <paramref name="upper"/> exclusively, <c>false</c> if not
+		/// </returns>
 		public static bool IsBetweenExclusive<T>(this T value, T lower, T upper) where T : IComparable<T>
 		{
 			return value.CompareTo(lower) > 0 && value.CompareTo(upper) < 0;
 		}
 
+		/// <summary>
+		/// Converts a fractional value to a percent
+		/// </summary>
+		/// <param name="multiplier">Value to convert</param>
+		/// <returns>The number as a percent</returns>
 		public static double ToPercent(this double multiplier)
 		{
 			return multiplier * 100.0;
 		}
-		public static double ToPercent(this int multiplier)
+		/// <summary>
+		/// Converts a fractional value to an integer percent
+		/// </summary>
+		/// <param name="multiplier">Value to convert</param>
+		/// <returns>The number as a percent</returns>
+		public static int ToPercentInt(this double multiplier)
 		{
-			return multiplier * 100.0;
+			return (int)(multiplier * 100.0);
 		}
 
+		/// <summary>
+		/// Returns a value x1024
+		/// </summary>
+		/// <param name="value">Value to multiply</param>
+		/// <returns><c><paramref name="value"/> * 1024</c></returns>
 		public static int K(this int value)
 		{
 			return value * 1024;
 		}
+		/// <summary>
+		/// Returns a value x1,048,576 (1024^2)
+		/// </summary>
+		/// <param name="value">Value to multiply</param>
+		/// <returns><c><paramref name="value"/> * 1048576</c></returns>
 		public static int M(this int value)
 		{
 			return value.K() * 1024;
 		}
+		/// <summary>
+		/// Returns a value x1,073,741,824 (1024^3)
+		/// </summary>
+		/// <param name="value">Value to multiply</param>
+		/// <returns><c><paramref name="value"/> * 1073741824</c></returns>
 		public static long G(this int value)
 		{
 			return value.M() * 1024L;
 		}
 
+		/// <summary>
+		/// Formats a number to a currency by culture
+		/// </summary>
+		/// <param name="value">Value to format</param>
+		/// <param name="cultureName">Culture to format to. Defaults to USD ($).</param>
+		/// <returns><paramref name="value"/> formatted into a currency</returns>
 		public static string ToCurrency(this double value, string cultureName = "en-US")
 		{
 			CultureInfo culture = new CultureInfo(cultureName);
 			return (string.Format(culture, "{0:C}", value));
 		}
 
+		/// <summary>
+		/// Returns whether an integer is an odd number
+		/// </summary>
+		/// <param name="value">Value to test</param>
+		/// <returns><c>true</c> if <paramref name="value"/> is odd, <c>false</c> if even</returns>
 		public static bool IsOdd(this int value)
 		{
 			return value % 2 == 1;
 		}
 
+		/// <summary>
+		/// Returns whether an integer is an even number
+		/// </summary>
+		/// <param name="value">Value to test</param>
+		/// <returns><c>true</c> if <paramref name="value"/> is even, <c>false</c> if odd</returns>
+		
 		public static bool IsEven(this int value)
 		{
 			return value % 2 == 0;
 		}
 
+		/// <summary>
+		/// Returns whether an integer is a multiple of a given number
+		/// </summary>
+		/// <param name="value">Value to test</param>
+		/// <param name="factor">Number to test if <paramref name="value"/> is a multiple of</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="value"/> is a multiple of <paramref name="factor"/>,
+		/// <c>false</c> if not.
+		/// </returns>
 		public static bool IsMultipleOf(this int value, int factor)
 		{
 			return value % factor == 0;
--- UltimateUtil/UltimateUtil/ReflectionUtil.cs
+++ UltimateUtil/UltimateUtil/ReflectionUtil.cs
@@ -9,18 +9,51 @@ using UltimateUtil.Fluid;
 
 namespace UltimateUtil
 {
+	/// <summary>
+	/// Various utilities for reflection
+	/// </summary>
 	public static class ReflectionUtil
 	{
+		/// <summary>
+		/// Returns whether a member has a given attribute
+		/// </summary>
+		/// <param name="tested">Member to test</param>
+		/// <param name="attribute">Type of attribute to look for</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="tested"/> has <paramref name="attribute"/> applied to it, 
+		/// <c>false</c> if not, or if <paramref name="attribute"/> is not a valid <see cref="Attribute"/>
+		/// </returns>
 		public static bool HasAttribute(this MemberInfo tested, Type attribute)
 		{
 			return tested.GetCustomAttributes(attribute, false).Length > 0;
 		}
+		/// <summary>
+		/// Returns whether a member has a given attribute
+		/// </summary>
+		/// <typeparam name="TAtt">Type of attribute to look for</typeparam>
+		/// <param name="tested">Member to test</param>
+		/// <returns>
+		/// <c>true</c> if <paramref name="tested"/> has <typeparamref name="TAtt"/> applied to it, 
+		/// <c>false</c> if not
+		/// </returns>
 		public static bool HasAttribute<TAtt>(this MemberInfo tested) where TAtt : Attribute
 		{
 			return tested.HasAttribute(typeof(TAtt));
 		}
 
-		public static List<Type> GetTypesWithAttribute(this Assembly a, Type attribute)
+		/// <summary>
+		/// Returns a list of all types in an assembly that have a given attribute applied to them
+		/// </summary>
+		/// <param name="assem">Assembly to search</param>
+		/// <param name="attribute">Type of attribute to look for</param>
+		/// <returns>
+		/// A list of all types within <paramref name="assem"/> that have <paramref name="attribute"/>
+		/// applied to them
+		/// </returns>
+		/// <exception cref="ArgumentException">
+		/// Thrown if <paramref name="attribute"/> does not inherit from <see cref="Attribute"/>
+		/// </exception>
+		public static List<Type> GetTypesWithAttribute(this Assembly assem, Type attribute)
 		{
 			if (!attribute.InheritsFrom<Attribute>())
 			{
@@ -28,7 +61,7 @@ namespace UltimateUtil
 			}
 
 			List<Type> res = new List<Type>();
-			foreach (Type t in a.GetTypes())
+			foreach (Type t in assem.GetTypes())
 			{
 				if (t.HasAttribute(attribute))
 				{
@@ -38,11 +71,33 @@ namespace UltimateUtil
 
 			return res;
 		}
-		public static List<Type> GetTypesWithAttribute<TAtt>(this Assembly a) where TAtt : Attribute
+		/// <summary>
+		/// Returns a list of all types in an assembly that have a given attribute applied to them
+		/// </summary>
+		/// <typeparam name="TAtt">Type of attribute to look for</typeparam>
+		/// <param name="assem">Assembly to search</param>
+		/// <returns>
+		/// A list of all types within <paramref name="assem"/> that have a <typeparamref name="TAtt"/>
+		/// applied to them
+		/// </returns>
+		public static List<Type> GetTypesWithAttribute<TAtt>(this Assembly assem) where TAtt : Attribute
 		{
-			return a.GetTypesWithAttribute(typeof(TAtt));
+			return assem.GetTypesWithAttribute(typeof(TAtt));
 		}
 
+		/// <summary>
+		/// Returns a list of all members in a type that have a given attribute applied to them
+		/// </summary>
+		/// <param name="type">Type within which to search</param>
+		/// <param name="att">Type of attribute to look for</param>
+		/// <param name="filters">Types of members to search for</param>
+		/// <returns>
+		/// A list of all members within <paramref name="type"/> that have <paramref name="att"/> applied
+		/// to them
+		/// </returns>
+		/// <exception cref="ArgumentException">
+		/// Thrown if <paramref name="att"/> does not inherit from <see cref="Attribute"/>
+		/// </exception>
 		public static List<MemberInfo> GetMembersWithAttribute(this Type type, Type att, MemberTypes filters = MemberTypes.All)
 		{
 			if (!att.InheritsFrom<Attribute>())
@@ -61,6 +116,16 @@ namespace UltimateUtil
 
 			return res;
 		}
+		/// <summary>
+		/// Returns a list of all members in a type that have a given attribute applied to them
+		/// </summary>
+		/// <typeparam name="TAtt">Type of attribute to look for</typeparam>
+		/// <param name="type">Type within which to search</param>
+		/// <param name="filters">Types of members to search for</param>
+		/// <returns>
+		/// A list of all members within <paramref name="type"/> that have <typeparamref name="TAtt"/>
+		/// applied to them
+		/// </returns>
 		public static List<MemberInfo> GetMembersWithAttribute<TAtt>(this Type type, MemberTypes filters = MemberTypes.All)
 			where TAtt : Attribute
 		{
@@ -99,12 +164,16 @@ namespace UltimateUtil
 		}
 
 		/// <summary>
-		/// Invokes an instance method by name
+		/// Invokes an instance method by string name. Useful for reflected types.
 		/// </summary>
 		/// <param name="obj">Object instance to invoke the method from. Cannot be null.</param>
 		/// <param name="methodName">Name of method to invoke.</param>
 		/// <param name="parameters">Parameters to supply the method</param>
 		/// <returns>Return value of the method</returns>
+		/// <exception cref="ArgumentException">
+		/// Thrown if <paramref name="obj"/> is <c>null</c> (indicating a static method), or if
+		/// there is no method found by the name of <paramref name="methodName"/>
+		/// </exception>
 		public static object InvokeMethod(this object obj, string methodName, params object[] parameters)
 		{
 			if (obj == null)
@@ -189,17 +258,49 @@ namespace UltimateUtil
 			return type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>));
 		}
 
+		/// <summary>
+		/// Gets the value stored in a private field in a type
+		/// </summary>
+		/// <param name="type">Type the field is in</param>
+		/// <param name="fieldName">Name of field</param>
+		/// <param name="instance">Instance of which to get the field from, <c>null</c> if <c>static</c></param>
+		/// <returns>The value stored in <paramref name="fieldName"/></returns>
 		public static object GetPrivateField(this Type type, string fieldName, object instance = null)
 		{
 			FieldInfo field = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
 			return field.GetValue(instance);
 		}
+		/// <summary>
+		/// Sets the value stored in a private field in a type
+		/// </summary>
+		/// <param name="type">Type the field is in</param>
+		/// <param name="fieldName">Name of field</param>
+		/// <param name="value">Value to set to. Must inherit from the field's type.</param>
+		/// <param name="instance">Instance of which to get the field from, <c>null</c> if <c>static</c></param>
+		/// <exception cref="ArgumentException">
+		/// Thrown if <paramref name="value"/> does not inherit from the field's type.
+		/// </exception>
 		public static void SetPrivateField(this Type type, string fieldName, object value, object instance = null)
 		{
 			FieldInfo field = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+			if (!value.GetType().InheritsFrom(field.FieldType))
+			{
+				throw new ArgumentException("Type {0} does not inherit from {1}."
+					.Fmt(value.GetType().FullName, field.FieldType.FullName), nameof(value));
+			}
+
 			field.SetValue(instance, value);
 		}
-		public static object RunPrivateMethod(this Type type, string methodName, object instance, params object[] args)
+
+		/// <summary>
+		/// Invokes a private method within a type
+		/// </summary>
+		/// <param name="type">Type the method is in</param>
+		/// <param name="methodName">Name of method</param>
+		/// <param name="instance">Instance of which to get the method from, or <c>null</c> if <c>static</c></param>
+		/// <param name="args">Parameters to supply the method</param>
+		/// <returns>The return value of the method, or null if it returns <see cref="void"/>.</returns>
+		public static object InvokePrivateMethod(this Type type, string methodName, object instance, params object[] args)
 		{
 			MethodInfo method = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
 			return method.Invoke(instance, args);
